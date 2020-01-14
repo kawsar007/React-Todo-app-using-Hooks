3d1034be5f7e24b1a09f310a74fe015576734428
kawsar007 <imkawsar007@gmail.com>
2020-01-14 07:00:18 -0800

    package.json

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import Todo from './todo';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function Todo({todo, index}) {
+// return <div className="todo">{todo.text}</div>
+// }
+
+const Todo = ({todo, index}) => {
+return <div className="todo">{todo.text}</div>
 }
 
+const App = () => {
+  const [ todos, setTodos ] = useState([
+    {
+       text: 'Learn about React',
+       isCompleted: false
+    },
+    {
+      text: 'Meet friend for lunch',
+      isCompleted: false
+   },
+   {
+    text: 'Build really cool todo app',
+    isCompleted: false
+   }
+  ])
+  return (
+    <div className="App">
+      <h2>Hello React Hooks</h2>
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo  key={index} index={index} todo={todo}/>
+        ))}
+      </div>
+    </div>
+  )
+}
 export default App;
---
+++ src/todo.js
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+// export default function Todo({todo, index}) {
+//     return (
+//         <div>
+//              <div className="todo">{todo.text}</div>
+//         </div>
+//     )
+// }
